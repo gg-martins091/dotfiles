2678827e399a3174b5a46f7be604276a435b0888
Gabriel Martins <gabriel.gmartins091@gmail.com>
2025-07-02 11:42:11 -0300

    fix tmux setup for macos

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@
 sudo apt install fonts-powerline ripgrep
 ln -s -f ~/dotfiles/nvim ~/.config/nvim
 ln -s ./terminator/config ~/.config/terminator/config
-ln -s ./tmux/.tmux.conf ~/.tmux.conf
 
+if [[ "$(uname)" == "Darwin" ]]; then
+	ln -s ./tmux/.tmux.conf.darwin ~/.tmux.conf
+else
+	ln -s ./tmux/.tmux.conf ~/.tmux.conf
+fi
